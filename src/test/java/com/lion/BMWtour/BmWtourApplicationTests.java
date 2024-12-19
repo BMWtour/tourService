49ed4eff0124d57af8fce9b926bf035e52145d9e
Laon0125 <87473656+Laon0125@users.noreply.github.com>
2024-12-19 16:37:06 +0900

    infra: 초기 gradle 세팅

---
+++ src/test/java/com/lion/BMWtour/BmWtourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.BMWtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmWtourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
