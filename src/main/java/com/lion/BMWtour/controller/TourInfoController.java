334d6e9a647ed8f669f06d03c988a1d30b7248e6
“mafatofu” <qwqw0310@gmail.com>
2024-12-23 17:55:43 +0900

    feat : 관광정보 상세페이지 기본 html 작성.
    기본 템플릿 작성
    네이버 지도 api로 관광지 좌표 나타나도록 추가

--- src/main/java/com/lion/BMWtour/controller/TourInfoController.java
+++ src/main/java/com/lion/BMWtour/controller/TourInfoController.java
@@ -1,11 +1,15 @@
 package com.lion.BMWtour.controller;
 
-import com.lion.BMWtour.service.TourInfoService;
 import com.lion.BMWtour.service.TourInfoServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClient;
+import org.yaml.snakeyaml.util.UriEncoder;
 
 
 @Controller
@@ -13,10 +17,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TourInfoController {
 
     private final TourInfoServiceImpl tourInfoService;
+    //naver map api client id
+    @Value("${naver.map.client-id}")
+    String mapClientId;
+    @Value("${odsay.map.secret-key}")
+    String odsaySecretKey;
 
     @GetMapping("/TourInfoInsert")
     public String CulturalPlaceInsert() {
         // tourInfoService.csvFileToElasticSearch();
         return "fragments/test";
+    }
+
+    /**상세페이지 테스트를 위한 컨트롤러
+     * html 페이지 확인을 위한 임시 컨트롤러
+     * 추후 삭제 예정
+     */
+    @GetMapping("/tour/detail")
+    public String detail(
+            Model model
+    ){
+
+        model.addAttribute("mapClientId", mapClientId);
+        model.addAttribute("odsaySecretKey", odsaySecretKey);
+        return "detail/detail";
+    }
+
+    /*odsay api 테스트용 컨트롤러
+    * 추후삭제예정*/
+    @GetMapping("/tour/findMapTest")
+    @ResponseBody
+    public ResponseEntity<String> findMapTest(){
+
+        RestClient restClient = RestClient.builder()
+                .baseUrl("https://api.odsay.com/v1/api")
+                .build();
+
+        /*
+        *
+        * https://api.odsay.com/v1/api/searchPubTransPathT
+        * ?lang=0&SX=126.9931257&SY=37.5680965&EX=126.8294568287&EY=37.3682496114&OPT=0&SearchType=0
+        * &apiKey=QF9aYGND2yFk+ITjGtpm0A
+        * */
+        ResponseEntity<String> response = restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/searchPubTransPathT")
+                        .queryParam("lang", 0)
+                        .queryParam("SX", 126.9931257)
+                        .queryParam("SY", 37.5680965)
+                        .queryParam("EX", 126.8294568287)
+                        .queryParam("EY", 37.3682496114)
+                        .queryParam("OPT", 0)
+                        .queryParam("SearchType", 0)
+                        .queryParam("apiKey",  "QF9aYGND2yFk%2BITjGtpm0A")
+                        .build())
+                .retrieve()
+                .toEntity(String.class);
+       ;
+        return response.getBody() == null ? ResponseEntity.noContent().build() : response;
     }
 }
