0725104e0bf898eece6fb82a4196e0e2b6dd4da5
wjdwndudghlw <wjdwndudghlw@naver.com>
2024-12-24 15:06:00 +0900

    feat: entity 업데이트 / tourinfoService 코드 수정 /문화관광데이터.csv 추가

--- src/main/java/com/lion/BMWtour/controller/TourInfoController.java
+++ src/main/java/com/lion/BMWtour/controller/TourInfoController.java
@@ -5,7 +5,11 @@ import com.lion.BMWtour.service.TourInfoServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -16,7 +20,15 @@ public class TourInfoController {
 
     @GetMapping("/TourInfoInsert")
     public String CulturalPlaceInsert() {
-        // tourInfoService.csvFileToElasticSearch();
+        tourInfoService.tourInfoInsert();
         return "fragments/test";
     }
+
+    @GetMapping("/search/{q}")
+    public String search(@PathVariable String q, Model model) {
+        System.out.println(q);
+        model.addAttribute("test",q);
+        return "fragments/test";
+    }
+
 }
