377e1834e840f39ce3ebc14e12a0e8f19ba551b4
Laon0125 <87473656+Laon0125@users.noreply.github.com>
2024-12-24 12:33:07 +0900

    파일 업로드 추가

---
+++ src/main/java/com/lion/BMWtour/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.lion.BMWtour.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(auth -> auth.disable())       // CSRF 방어 기능 비활성화
+                .headers(x -> x.frameOptions(y -> y.disable()))     // H2-console
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/book/list", "/book/detail/**", "/bookEs/list", "/bookEs/detail/**","/bookEs/listOld",
+                                "/misc/**", "/webSocket", "/echo", "/personal", "/restaurant/list", "/restaurant/detail/**",
+                                "/mall/list", "/mall/detail/**", "/user/register", "/h2-console", "/demo/**",
+                                "/img/**", "/js/**", "/css/**", "/error/**","/api/gcs/upload"
+                        ).permitAll()
+                        .requestMatchers(
+                                //"/book/insert", "/book/yes24",
+                                "/order/listAll", "/order/bookStat", "/bookEs/yes24", "/restaurant/init",
+                                "/user/delete", "/user/list").hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .logout(auth -> auth
+                        .logoutUrl("/user/logout")
+                        .invalidateHttpSession(true)        // 로그아웃시 세션 삭제
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl("/user/login")
+                )
+        ;
+
+        return http.build();
+    }
+
+
+    // Authentication Manager 빈 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/lion/BMWtour/service/FileUploadService.java
@@ -0,0 +1,37 @@
+package com.lion.BMWtour.service;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.lion.BMWtour.request.FileRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class FileUploadService {
+
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    private String bucketName;
+
+    private final Storage storage; // Spring Bean으로 관리되는 Storage 인스턴스를 주입받음
+
+    public String uploadFile(FileRequest request) throws IOException {
+
+        // !!!!!!!!!!!이미지 업로드 관련 부분!!!!!!!!!!!!!!!
+        String uuid = UUID.randomUUID().toString(); // Google Cloud Storage에 저장될 파일 이름
+        String ext = request.getFile().getContentType(); // 파일의 형식 ex) JPG
+        // Cloud에 이미지 업로드
+        BlobInfo blobInfo = storage.create(
+                BlobInfo.newBuilder(bucketName, uuid)
+                        .setContentType(ext)
+                        .build(),
+                request.getFile().getInputStream()
+        );
+        return uuid;
+    }
+}
