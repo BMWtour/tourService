636bb3cbe8e036da7f17303bc854a0f50442562b
“mafatofu” <qwqw0310@gmail.com>
2024-12-27 10:06:50 +0900

    fix : TourInfoController git conflict 해결

--- src/main/java/com/lion/BMWtour/controller/TourInfoController.java
+++ src/main/java/com/lion/BMWtour/controller/TourInfoController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestClient;
 import org.yaml.snakeyaml.util.UriEncoder;
-
 import java.net.URLEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -29,22 +29,25 @@ public class TourInfoController {
 
     @GetMapping("/TourInfoInsert")
     public String CulturalPlaceInsert() {
-        // tourInfoService.csvFileToElasticSearch();
+        tourInfoService.tourInfoInsert();
         return "fragments/test";
     }
 
-    /**상세페이지 테스트를 위한 컨트롤러
-     * html 페이지 확인을 위한 임시 컨트롤러
-     * 추후 삭제 예정
-     */
+    /**상세페이지 테스트를 위한 컨트롤러*/
     @GetMapping("/tour/detail")
     public String detail(
             Model model
     ){
-
         model.addAttribute("mapClientId", mapClientId);
         model.addAttribute("odsaySecretKey", odsaySecretKey);
         return "detail/detail";
+    }
+
+    @GetMapping("/search/{q}")
+    public String search(@PathVariable String q, Model model) {
+        System.out.println(q);
+        model.addAttribute("test",q);
+        return "fragments/test";
     }
 
 }
