1c953eb8e52b997fbba9503cd280b5640f5f673f
jsjseop <57559288+jsjseop@users.noreply.github.com>
2025-01-03 15:39:00 +0900

    feat: 카테고리별 인기 관광지 조회 기능 구현

--- src/main/java/com/lion/BMWtour/service/MainServiceImpl.java
+++ src/main/java/com/lion/BMWtour/service/MainServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.lion.BMWtour.dto.main.NearbyLocationResponse;
+import com.lion.BMWtour.dto.main.PopularByCategoryResponse;
 import com.lion.BMWtour.dto.main.PopularRegionsResponse;
 import com.lion.BMWtour.entity.TourInfo;
 import com.lion.BMWtour.entity.TourLog;
@@ -15,8 +16,11 @@ import co.elastic.clients.elasticsearch._types.GeoDistanceSort;
 import co.elastic.clients.elasticsearch._types.LatLonGeoLocation;
 import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
+import co.elastic.clients.elasticsearch._types.aggregations.StringTermsBucket;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.HitsMetadata;
+import co.elastic.clients.json.JsonData;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -63,6 +67,81 @@ public class MainServiceImpl implements MainService {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	@Override
+	public List<PopularByCategoryResponse> getPopularByCategoryList() {
+		SearchRequest searchRequest = createPopularToursByCategorySearchRequest();
+
+		try {
+			SearchResponse<TourLog> search = elasticsearchClient.search(searchRequest, TourLog.class);
+
+			List<StringTermsBucket> groupByCategory = search.aggregations().get("groupByCategory").sterms().buckets().array();
+			return groupByCategory.stream()
+				.map(categoryBucket -> {
+					List<StringTermsBucket> groupById = categoryBucket.aggregations().get("groupById").sterms().buckets().array();
+					HitsMetadata<JsonData> topTour = groupById.get(0).aggregations().get("topTour").topHits().hits();
+					return topTour.hits().get(0).source().to(TourLog.class);
+				})
+				.map(tourLog -> PopularByCategoryResponse.builder()
+					.category(tourLog.getCategory())
+					.id(tourLog.getTourId())
+					.title(tourLog.getTourTitle())
+					.address(tourLog.getTourAddress())
+					.image("/img/default/category/" + tourLog.getCategory() + ".jpg")
+					.build())
+				.toList();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static SearchRequest createPopularToursByCategorySearchRequest() {
+		// {
+		// 	"size": 0,
+		// 	"aggs": {
+		// 	"groupByCategory": {
+		// 		"terms": {
+		// 			"field": "category",
+		// 			"size": 6
+		// 		},
+		// 		"aggs": {
+		// 			"groupById": {
+		// 				"terms": {
+		// 					"field": "tourId",
+		// 					"size": 1
+		// 				},
+		// 				"aggs": {
+		// 					"topTour": {
+		// 						"top_hits": {
+		// 							"size": 1
+		// 						}
+		// 					}
+		// 				}
+		// 			}
+		// 		}
+		// 	 }
+		//  }
+		// }
+		return new SearchRequest.Builder()
+			.index("tourlogs")
+			.size(0)
+			.aggregations("groupByCategory", Aggregation.of(a ->
+				a.terms(
+						t -> t.field("category")
+							.size(MAIN_PAGE_ITEM_COUNT)
+					)
+					.aggregations("groupById", Aggregation.of(subA ->
+						subA.terms(
+								subT -> subT.field("tourId")
+									.size(1)
+							)
+							.aggregations("topTour", Aggregation.of(topA ->
+								topA.topHits(th -> th.size(1)))
+							)
+					))
+			))
+			.build();
 	}
 
 	@Override
