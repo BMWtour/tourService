80bfd2c2992b128913304fedb5b9169fcd3a1dd9
wjdwndudghlw <wjdwndudghlw@naver.com>
2025-01-06 16:50:23 +0900

    feat: 검색 페이지 UI 변경 및 정렬 컨테이너 추가

--- src/main/java/com/lion/BMWtour/controller/TourInfoController.java
+++ src/main/java/com/lion/BMWtour/controller/TourInfoController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
